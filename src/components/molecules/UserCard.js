c8ed7c002043a94e494f793647a17053355f7c2c
senuraa <senuraa@msn.com>
2021-04-13 21:34:45 +0530

    Replaced Users class component with functional component

--- src/components/molecules/UserCard.js
+++ src/components/molecules/UserCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import UserCardHeader from "../molecules/UserCardHeader";
+import UserCardHeader from "./UserCardHeader";
 import { shape, string } from "prop-types";
 import styled from "styled-components";
 
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,92 +1,71 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { func, shape, string } from "prop-types";
-import { Row, Col, Container } from "react-grid-system";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../actions/users";
-import Typography from "../components/atoms/Typography";
-import UserCard from "../components/organisms/UserCard";
+import { Col, Container, Row } from "react-grid-system";
+import UserCard from "../components/molecules/UserCard";
 import styled from "styled-components";
+import Typography from "../components/atoms/Typography";
 
 const DepTitle = styled(Typography)`
   margin: 1rem;
 `;
 
-class Users extends Component {
-  constructor(props) {
-    super(props);
-    this.createUsersList = this.createUsersList.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.getUsers();
-  }
-
-  createUsersList() {
-    const { isLoading, isFailed, data } = this.props.users;
-    let deps = [];
-    if (!isLoading && !isFailed && data.length > 0) {
-      let usersGrouped = data.reduce((r, a) => {
-        r[a.departmentName] = r[a.departmentName] || [];
-        r[a.departmentName].push(a);
-        return r;
-      }, Object.create(null));
-      Object.keys(usersGrouped).map((dep) => {
-        let users = [];
-        usersGrouped[dep].map((user) => {
-          users.push(
-            <Col sm={3} key={user._id}>
-              <UserCard user={user} />
-            </Col>
-          );
-        });
-        deps.push(
-          <div key={`${dep}_title`}>
-            <Row>
-              <Col>
-                <DepTitle color={"quaternary"} variant={"h3"}>
-                  {dep}
-                </DepTitle>
-              </Col>
-            </Row>
-            <Row key={`${dep}`}>{users}</Row>
-          </div>
-        );
-      });
-      return deps;
-    }
-  }
-
-  render() {
-    const { isLoading, isFailed, data } = this.props.users;
-    if (!isLoading && !isFailed && data.length > 0) {
-      return <Container>{this.createUsersList()}</Container>;
-    } else if (isLoading) {
-      return (
-        <Typography color={"primary"} variant={"h1"} align={"center"}>
-          Loading...
-        </Typography>
+const createUsersList = (users) => {
+  let deps = [];
+  let usersGrouped = users.reduce((r, a) => {
+    r[a.departmentName] = r[a.departmentName] || [];
+    r[a.departmentName].push(a);
+    return r;
+  }, Object.create(null));
+  Object.keys(usersGrouped).map((dep) => {
+    let users = [];
+    usersGrouped[dep].map((user) => {
+      users.push(
+        <Col sm={3} key={user._id}>
+          <UserCard user={user} />
+        </Col>
       );
-    } else {
-      return (
-        <Typography color={"primary"} variant={"h1"} align={"center"}>
-          Failed
-        </Typography>
-      );
-    }
-  }
-}
-
-Users.propTypes = {
-  users: shape({
-    test: string,
-  }),
-  getUsers: func,
+    });
+    deps.push(
+      <div key={`${dep}_title`}>
+        <Row>
+          <Col>
+            <DepTitle color={"quaternary"} variant={"h3"}>
+              {dep}
+            </DepTitle>
+          </Col>
+        </Row>
+        <Row key={`${dep}`}>{users}</Row>
+      </div>
+    );
+  });
+  return deps;
 };
 
-const mapStateToProps = (state) => {
-  return {
-    users: state.users,
-  };
+const Users = () => {
+  const users = useSelector((state) => state.users);
+  const { data, isLoading, isFailed } = users;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
+  if (!isLoading && !isFailed && data.length > 0) {
+    return <Container>{createUsersList(data)}</Container>;
+  } else if (isLoading) {
+    return (
+      <Typography color={"primary"} variant={"h1"} align={"center"}>
+        Loading...
+      </Typography>
+    );
+  } else {
+    return (
+      <Typography color={"primary"} variant={"h1"} align={"center"}>
+        Failed
+      </Typography>
+    );
+  }
 };
 
-export default connect(mapStateToProps, { getUsers })(Users);
+Users.propTypes = {};
+
+export default Users;
